fa17a6a69cc33a5f99fd1a0be67cdd58a254ffb5
Elle Brooks <hello@ellebell.dev>
2026-07-13 12:47:06 -0700

    Refactor from Bourke conventions to Lengyel conventions
    Marching cube algorithm has been refactored to use Lengyel's conventions from the transvoxel algorithm instead of the normal Bourke convention. The chunks.gd file now references the data in transvoxel-lut.gd and the marching-cubes-lut.gd is now removed from the project.
    
    *So many bitwise operation. AHHHHH*

--- environment/world-generation/chunk.gd
+++ environment/world-generation/chunk.gd
@@ -59,34 +59,18 @@ func _construct_sample_set() -> PackedFloat32Array:
 
 ## Uses the scalar property and isosurface property to generate mesh vertices and normals. Assigns the generated data to mesh_vertices and mesh_data
 func _generate_mesh_data(scalar_samples: PackedFloat32Array) -> ArrayMesh:
-	#    c4---------e4-------------c5
-	# e6 / |                    e5 /|
-	#   /  |                      / | 
-	# c7------------e6----------c6  |
-	#  |   |                     |  |
-	#  |   e8                    |  e9
-	#  |   |                     |  |
-	# e11  |                    e10 |
-	#  |  c0----------e0---------|-c1
-	#  |  / e3                   | /
-	#  | /                       |/ e1
-	# c3-----------e2-----------c2
-	# 8-bit case index = c0*1 + c1*2 + c2*4 + c3*8 + c4*16 + c5*32 + c6*64 + c7*128
-	# the two corners each edge connects
-	const EDGE_CORNERS := [
-		[0, 1],
-		[1, 2],
-		[2, 3],
-		[3, 0],
-		[4, 5],
-		[5, 6],
-		[6, 7],
-		[7, 4],
-		[0, 4],
-		[1, 5],
-		[2, 6],
-		[3, 7]
-	]
+	#    c6----------------------c7
+	#    / |                     /|
+	#   /  |                    / | 
+	# c4----------------------c5  |
+	#  |   |                   |  |
+	#  |   |                   |  |  
+	#  |   |                   |  |
+	#  |   |                   |  |
+	#  |  c2-------------------|-c3
+	#  |  /                    | /
+	#  | /                     |/ 
+	# c0----------------------c1
 	# Arrays for storing generated mesh data
 	var mesh_vertices: PackedVector3Array = []
 	var mesh_normals: PackedVector3Array = []
@@ -101,68 +85,110 @@ func _generate_mesh_data(scalar_samples: PackedFloat32Array) -> ArrayMesh:
 				var x_stride := int(pow(size + 1, 2))
 				var c0 = scalar_samples[x * x_stride + y * y_stride + z]
 				var c1 = scalar_samples[(x + 1) * x_stride + y * y_stride + z]
-				var c2 = scalar_samples[(x + 1) * x_stride + y * y_stride + (z + 1)]
-				var c3 = scalar_samples[x * x_stride + y * y_stride + (z + 1)]
+				var c2 = scalar_samples[x * x_stride + y * y_stride + (z + 1)]
+				var c3 = scalar_samples[(x + 1) * x_stride + y * y_stride + (z + 1)]
 				var c4 = scalar_samples[x * x_stride + (y + 1) * y_stride + z]
 				var c5 = scalar_samples[(x + 1) * x_stride + (y + 1) * y_stride + z]
-				var c6 = scalar_samples[(x + 1) * x_stride + (y + 1) * y_stride + (z + 1)]
-				var c7 = scalar_samples[x * x_stride + (y + 1) * y_stride + (z + 1)]
+				var c6 = scalar_samples[x * x_stride + (y + 1) * y_stride + (z + 1)]
+				var c7 = scalar_samples[(x + 1) * x_stride + (y + 1) * y_stride + (z + 1)]
 				var corner_scalars = [c0, c1, c2, c3, c4, c5, c6, c7]
 				
 				# cube corner positions in space
 				var c0_pos = Vector3(x, y, z) * lod_step
 				var c1_pos = Vector3((x + 1), y, z) * lod_step
-				var c2_pos = Vector3((x + 1), y, (z + 1)) * lod_step
-				var c3_pos = Vector3(x, y, (z + 1)) * lod_step
+				var c2_pos = Vector3(x, y, (z + 1)) * lod_step
+				var c3_pos = Vector3((x + 1), y, (z + 1)) * lod_step
 				var c4_pos = Vector3(x, (y + 1), z) * lod_step
 				var c5_pos = Vector3((x + 1), (y + 1), z) * lod_step
-				var c6_pos = Vector3((x + 1), (y + 1), (z + 1)) * lod_step
-				var c7_pos = Vector3(x, (y + 1), (z + 1)) * lod_step
+				var c6_pos = Vector3(x, (y + 1), (z + 1)) * lod_step
+				var c7_pos = Vector3((x + 1), (y + 1), (z + 1)) * lod_step
 				var corner_pos = [c0_pos, c1_pos, c2_pos, c3_pos, c4_pos, c5_pos, c6_pos, c7_pos]
 				
-				# determine case index
-				var case_index = 0
-				if c0 < isosurface: case_index |= (1 << 0)
-				if c1 < isosurface: case_index |= (1 << 1)
-				if c2 < isosurface: case_index |= (1 << 2)
-				if c3 < isosurface: case_index |= (1 << 3)
-				if c4 < isosurface: case_index |= (1 << 4)
-				if c5 < isosurface: case_index |= (1 << 5)
-				if c6 < isosurface: case_index |= (1 << 6)
-				if c7 < isosurface: case_index |= (1 << 7)
-				var segment = MarchingCubesLUT.TRI_TABLE[case_index]
-				
-				# determine where mesh vertices go in real space and determine normals for each mesh vertex (uses flat shading on purpose)
-				var i := 0
-				while i < segment.size() and segment[i] != -1:
-					var edge_a = segment[i]
-					var edge_b = segment[i + 1]
-					var edge_c = segment[i + 2]
-					var vertex_a: Vector3
-					var vertex_b: Vector3
-					var vertex_c: Vector3
-					
+				# determine regular class index
+				var class_index = 0
+				if c0 < isosurface: class_index |= (1 << 0)
+				if c1 < isosurface: class_index |= (1 << 1)
+				if c2 < isosurface: class_index |= (1 << 2)
+				if c3 < isosurface: class_index |= (1 << 3)
+				if c4 < isosurface: class_index |= (1 << 4)
+				if c5 < isosurface: class_index |= (1 << 5)
+				if c6 < isosurface: class_index |= (1 << 6)
+				if c7 < isosurface: class_index |= (1 << 7)
+				# Skip empty cells
+				if class_index == 0 or class_index == 255:
+					continue
+
+				# use class_index to determine the cell class, construct cell data, and get vertex count
+				var cell_class: int = TransvoxelLUT.REG_CELL_CLASS[class_index]
+				var vertex_count := TransvoxelLUT.get_vertex_count(TransvoxelLUT.REG_CELL_DATA[cell_class][0])
+
+				# Construct a set of vertices to be used for this cell
+				var vertex_set: Array[Vector3] = []
+				for i in vertex_count:
+					var vertex_data: int = TransvoxelLUT.REG_VERTEX_DATA[class_index][i]
+					# Use data in the low byte to construct an edge
+					var low_byte := vertex_data & 0xFF
+					var corner_a := low_byte >> 4
+					var corner_b := low_byte & 0x0F
+
 					# determine interpolation ratio
-					var t_a = (isosurface - corner_scalars[EDGE_CORNERS[edge_a][0]]) / (corner_scalars[EDGE_CORNERS[edge_a][1]] - corner_scalars[EDGE_CORNERS[edge_a][0]])
-					var t_b = (isosurface - corner_scalars[EDGE_CORNERS[edge_b][0]]) / (corner_scalars[EDGE_CORNERS[edge_b][1]] - corner_scalars[EDGE_CORNERS[edge_b][0]])
-					var t_c = (isosurface - corner_scalars[EDGE_CORNERS[edge_c][0]]) / (corner_scalars[EDGE_CORNERS[edge_c][1]] - corner_scalars[EDGE_CORNERS[edge_c][0]])
-					# apply interpolation ratio when determining vertex position
-					vertex_a = corner_pos[EDGE_CORNERS[edge_a][0]].lerp(corner_pos[EDGE_CORNERS[edge_a][1]], t_a)
-					vertex_b = corner_pos[EDGE_CORNERS[edge_b][0]].lerp(corner_pos[EDGE_CORNERS[edge_b][1]], t_b)
-					vertex_c = corner_pos[EDGE_CORNERS[edge_c][0]].lerp(corner_pos[EDGE_CORNERS[edge_c][1]], t_c)
-					
+					var interpolation_ratio: float = (isosurface - corner_scalars[corner_a]) / (corner_scalars[corner_b] - corner_scalars[corner_a])
+					var vertex: Vector3 = corner_pos[corner_a].lerp(corner_pos[corner_b], interpolation_ratio)
+
+					vertex_set.append(vertex)
+				
+				## Using the vertex set and cell_data append vertices to the mesh_vertices array to create triangles and calculate flat normals.
+				var vertex_indices = TransvoxelLUT.REG_CELL_DATA[cell_class][1]
+				var i := 0
+				while i < vertex_indices.size():
+					var vertex_a = vertex_set[vertex_indices[i]]
+					var vertex_b = vertex_set[vertex_indices[i + 1]]
+					var vertex_c = vertex_set[vertex_indices[i + 2]]
+
 					# append vertices to vertices array
 					mesh_vertices.append(vertex_a)
-					mesh_vertices.append(vertex_c)
 					mesh_vertices.append(vertex_b)
+					mesh_vertices.append(vertex_c)
 					
 					# determine normal and append to normals array
-					var normal = (vertex_b - vertex_a).cross(vertex_c - vertex_a)
+					var normal = - (vertex_b - vertex_a).cross(vertex_c - vertex_a)
 					mesh_normals.append(normal)
 					mesh_normals.append(normal)
 					mesh_normals.append(normal)
 					
 					i += 3
+				
+				# # determine where mesh vertices go in real space and determine normals for each mesh vertex (uses flat shading on purpose)
+				# var i := 0
+				# while i < triangulation_data.size() and triangulation_data[i] != -1:
+				# 	var edge_a = segment[i]
+				# 	var edge_b = segment[i + 1]
+				# 	var edge_c = segment[i + 2]
+				# 	var vertex_a: Vector3
+				# 	var vertex_b: Vector3
+				# 	var vertex_c: Vector3
+					
+				# 	# determine interpolation ratio
+				# 	var t_a = (isosurface - corner_scalars[EDGE_CORNERS[edge_a][0]]) / (corner_scalars[EDGE_CORNERS[edge_a][1]] - corner_scalars[EDGE_CORNERS[edge_a][0]])
+				# 	var t_b = (isosurface - corner_scalars[EDGE_CORNERS[edge_b][0]]) / (corner_scalars[EDGE_CORNERS[edge_b][1]] - corner_scalars[EDGE_CORNERS[edge_b][0]])
+				# 	var t_c = (isosurface - corner_scalars[EDGE_CORNERS[edge_c][0]]) / (corner_scalars[EDGE_CORNERS[edge_c][1]] - corner_scalars[EDGE_CORNERS[edge_c][0]])
+				# 	# apply interpolation ratio when determining vertex position
+				# 	vertex_a = corner_pos[EDGE_CORNERS[edge_a][0]].lerp(corner_pos[EDGE_CORNERS[edge_a][1]], t_a)
+				# 	vertex_b = corner_pos[EDGE_CORNERS[edge_b][0]].lerp(corner_pos[EDGE_CORNERS[edge_b][1]], t_b)
+				# 	vertex_c = corner_pos[EDGE_CORNERS[edge_c][0]].lerp(corner_pos[EDGE_CORNERS[edge_c][1]], t_c)
+					
+				# 	# append vertices to vertices array
+				# 	mesh_vertices.append(vertex_a)
+				# 	mesh_vertices.append(vertex_c)
+				# 	mesh_vertices.append(vertex_b)
+					
+				# 	# determine normal and append to normals array
+				# 	var normal = (vertex_b - vertex_a).cross(vertex_c - vertex_a)
+				# 	mesh_normals.append(normal)
+				# 	mesh_normals.append(normal)
+				# 	mesh_normals.append(normal)
+					
+				# 	i += 3
 	
 	# package data needed to create the mesh
 	var mesh_indices: PackedInt32Array = []
